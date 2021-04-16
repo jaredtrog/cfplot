597046bad9efbc5fb847cc30f87db51da11f398f
Jared Trog <jared.trog@gmail.com>
2021-04-16 16:29:53 -0500

    Get all cloudformation events

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 It was increasingly cumbersome to eye-grep CF output in the AWS console. I couldn't find another tool out there to provide individual resource deployment time so I wrote this. Please use at your own risk. No warranties expressed or implied.
 
+This will connect to AWS, grab the output of describe_stacks and generate a horizontal waterfall diagram similar to a webpage load waterfall diagram.
+
 ## Requirements
 
 * An AWS profile configured locally
--- cfplot.py
+++ cfplot.py
@@ -8,65 +8,75 @@ def format_time_from_seconds(seconds):
     minutes, seconds = divmod(remainder, 60)
     return f'{hours:02}:{minutes:02}:{seconds:02}'
 
+def order_events(events):
+    # Reverse the order of pages
+    for e in events:
+        e['StackEvents'].reverse()
+    events.reverse()
+    return events
+
 def main(stackname, profile='default', region='us-east-2'):
     session = boto3.session.Session(
         profile_name=profile, region_name=region)
     cloudformation_client = session.client('cloudformation')
-    response = cloudformation_client.describe_stack_events(
+    paginator = cloudformation_client.get_paginator('describe_stack_events')
+    response = paginator.paginate(
         StackName=stackname)
     first = True
     start = None
     data = {}
-    # Reverse the order to get events oldest to newest
-    response['StackEvents'].reverse()
-    for r in response['StackEvents']:
-        if first:
-            start = r['Timestamp']
-            first = False
-        # Initialize the waterfall for this Resource
-        if r['LogicalResourceId'] not in data:
-            base = r['Timestamp'] - start
-            data[r['LogicalResourceId']] = {
-                'result': {
-                    'x': [],
-                    'y': [],
-                    'text': [],
-                    'textfont': {"family": "Open Sans, light",
-                                 "color": "black"
-                                 },
-                    'textposition': "outside",
-                    'width': 0.5,
-                    'base': base.seconds,
-                    'measure': [],
-                    'increasing': {"marker": {"color": "Teal"}}},
-                'start_time': r['Timestamp']}
-        # Calculate the distance from stack start
-        duration = r['Timestamp'] - data[r['LogicalResourceId']]['start_time']
-        # If there are no values recorded for this resource ID, set the first
-        # one as an absolute. TODO: this may need to change.
-        if len(data[r['LogicalResourceId']]['result']['measure']) == 0:
-            data[r['LogicalResourceId']
-                 ]['result']['measure'].append('absolute')
-        # Otherwise set it as relative
-        else:
-            data[r['LogicalResourceId']
-                 ]['result']['measure'].append('relative')
-        # Record the results
-        data[r['LogicalResourceId']]['result']['x'].append(
-            duration.seconds)
-        data[r['LogicalResourceId']]['result']['y'].append(
-            r['LogicalResourceId'])
-        
-        # If this is the last event then set a text label for the
-        # total time the resource took to deploy.
-        if r['ResourceStatus'] == 'CREATE_COMPLETE':
-            resource_duration = sum(data[r['LogicalResourceId']]['result']['x'])
-            data[r['LogicalResourceId']]['result']['text'].append(
-                format_time_from_seconds(resource_duration))
-        else:
-            data[r['LogicalResourceId']]['result']['text'].append(
-                '')
+    # Drop it, flip it, and reverse it.
+    events = order_events([e for e in response])
+    for page in events:
+        for event in page['StackEvents']:
+            if first:
+                start = event['Timestamp']
+                first = False
+            # Initialize the waterfall for this Resource
+            if event['LogicalResourceId'] not in data:
+                base = event['Timestamp'] - start
+                data[event['LogicalResourceId']] = {
+                    'result': {
+                        'x': [],
+                        'y': [],
+                        'text': [],
+                        'textfont': {"family": "Open Sans, light",
+                                    "color": "black"
+                                    },
+                        'textposition': "outside",
+                        'width': 0.5,
+                        'base': base.seconds,
+                        'measure': [],
+                        'increasing': {"marker": {"color": "Teal"}}},
+                    'start_time': event['Timestamp']}
+            # Calculate the distance from stack start
+            duration = event['Timestamp'] - data[event['LogicalResourceId']]['start_time']
+            # If there are no values recorded for this resource ID, set the first
+            # one as an absolute. TODO: this may need to change.
+            if len(data[event['LogicalResourceId']]['result']['measure']) == 0:
+                data[event['LogicalResourceId']
+                    ]['result']['measure'].append('absolute')
+            # Otherwise set it as relative
+            else:
+                data[event['LogicalResourceId']
+                    ]['result']['measure'].append('relative')
+            # Record the results
+            data[event['LogicalResourceId']]['result']['x'].append(
+                duration.seconds)
+            data[event['LogicalResourceId']]['result']['y'].append(
+                event['LogicalResourceId'])
+            
+            # If this is the last event then set a text label for the
+            # total time the resource took to deploy.
+            if event['ResourceStatus'] == 'CREATE_COMPLETE':
+                resource_duration = sum(data[event['LogicalResourceId']]['result']['x'])
+                data[event['LogicalResourceId']]['result']['text'].append(
+                    format_time_from_seconds(resource_duration))
+            else:
+                data[event['LogicalResourceId']]['result']['text'].append(
+                    '')
 
+    # Format the total run time for display
     total_time = format_time_from_seconds(data[stackname]["result"]["x"][-1])
     
     fig = go.Figure()
